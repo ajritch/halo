899913d0a7ba0605f460cba1d288cf237a83c657
jkspam2004 <jkspam2004@yahoo.com>
2016-09-24 13:00:31 -0700

    add gitignore and updated package.json and bower.json files

---
+++ server/config/routes.js
@@ -0,0 +1,17 @@
+console.log("server routes");
+/* require controller file */
+var users = require('../controllers/users.js');
+var individuals = require('../controllers/individuals.js');
+
+module.exports = function(app) {
+  /* CRUD routes for users, only needed ones */
+  app.get('/users', users.index);
+  app.get('/users/:username', users.show);
+  app.post('/users', users.create);
+
+  /* CRUD routes for individuals */
+  app.get('/individuals', individuals.index);
+  app.get('/individuals/:id', individuals.show);
+  app.post('/individuals', individuals.create);
+}
+
